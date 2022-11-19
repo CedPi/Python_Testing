14197f8f853ff3e009ecad5794d61b63ee21407d
Cedric Pinson <a.cedric.pinson@gmail.com>
2022-11-19 05:07:26 +0100

    Ajout test email vide

--- tests/unitaires/test_connexion.py
+++ tests/unitaires/test_connexion.py
@@ -8,3 +8,9 @@ def test_email_should_fail(client):
     email = 'dummy@test.com'
     response = client.post('/showSummary', data={'email': email}, follow_redirects=True)
     assert response.status_code == 403
+
+
+def test_email_is_empty(client):
+    email = ''
+    response = client.post('/showSummary', data={'email': email}, follow_redirects=True)
+    assert response.status_code == 403
